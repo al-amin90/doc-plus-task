e277f3b4f2c3f35b62a1acc065cd660348387bce
al-amin90 <ijesun30@gmail.com>
2024-07-03 12:45:48 +0600

    first commit

---
+++ src/MainLayout/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "../Pages/Home/Home";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>I am main layout</h1>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default Main;
